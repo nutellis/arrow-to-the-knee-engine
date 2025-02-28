acd39f21bb2d25d3b3198cff0017c9e25bd1be07
Christos Ringas <xristosriggas2@gmail.com>
2025-02-28 15:30:42 +0100

    ecs-v2

--- Shard/ConsoleApp1/obj/x64/Debug/net8.0-windows/Shard.AssemblyInfo.cs
+++ Shard/ConsoleApp1/obj/x64/Debug/net8.0-windows/Shard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef32451361d00c924157be605b2dcd454957d18d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18da65e583387ed25d1c3362c0d658506e6073d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
